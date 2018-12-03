d9278ccbb8fee5adfb8641ee12e8dea8cd05d006
lmjing <lmjing1234@gmail.com>
2018-12-03 13:45:38 +0900

    기본 setting

---
+++ app/src/main/java/com/kt/iotheroes/kidscafesolution/Splash/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.kt.iotheroes.kidscafesolution.Splash;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.kt.iotheroes.kidscafesolution.R;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+    }
+
+}
