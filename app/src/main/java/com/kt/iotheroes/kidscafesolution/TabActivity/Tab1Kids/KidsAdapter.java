67ee54121844e8ba0465fe45edeb0aeac0dbc57a
lmjing <lmjing1234@gmail.com>
2018-12-04 15:49:53 +0900

    kid view holder 정의

--- app/src/main/java/com/kt/iotheroes/kidscafesolution/TabActivity/Tab1Kids/KidsAdapter.java
+++ app/src/main/java/com/kt/iotheroes/kidscafesolution/TabActivity/Tab1Kids/KidsAdapter.java
@@ -1,5 +1,6 @@
 package com.kt.iotheroes.kidscafesolution.TabActivity.Tab1Kids;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
 
@@ -8,6 +9,9 @@ import android.view.ViewGroup;
  */
 
 public class KidsAdapter extends RecyclerView.Adapter<KidsViewHolder> {
+
+    private Context context;
+
     @Override
     public KidsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return null;
--- app/src/main/java/com/kt/iotheroes/kidscafesolution/TabActivity/Tab1Kids/KidsViewHolder.java
+++ app/src/main/java/com/kt/iotheroes/kidscafesolution/TabActivity/Tab1Kids/KidsViewHolder.java
@@ -1,7 +1,10 @@
 package com.kt.iotheroes.kidscafesolution.TabActivity.Tab1Kids;
 
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.kt.iotheroes.kidscafesolution.R;
 import com.kt.iotheroes.kidscafesolution.Util.Dialog.ViewHolderParent;
 
 /**
@@ -9,9 +12,17 @@ import com.kt.iotheroes.kidscafesolution.Util.Dialog.ViewHolderParent;
  */
 
 public class KidsViewHolder extends ViewHolderParent {
-
+    TextView text_name, text_band, text_age, text_height, text_weight;
+    ImageView image_sex;
 
     public KidsViewHolder(View itemView) {
         super(itemView);
+
+        text_name = (TextView) itemView.findViewById(R.id.text_name);
+        text_band = (TextView) itemView.findViewById(R.id.text_band);
+        text_age = (TextView) itemView.findViewById(R.id.text_age);
+        text_height = (TextView) itemView.findViewById(R.id.text_height);
+        text_weight = (TextView) itemView.findViewById(R.id.text_weight);
+        image_sex = (ImageView) itemView.findViewById(R.id.image_sex);
     }
 }
