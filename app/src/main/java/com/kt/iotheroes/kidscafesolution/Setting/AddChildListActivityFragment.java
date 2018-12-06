fef28f7f10bfd79a569713e7c0a371dacd8f89ad
lmjing <lmjing1234@gmail.com>
2018-12-06 17:35:00 +0900

    불필요한 부분 지움

--- app/src/main/java/com/kt/iotheroes/kidscafesolution/Setting/AddChildListActivityFragment.java
+++ app/src/main/java/com/kt/iotheroes/kidscafesolution/Setting/AddChildListActivityFragment.java
@@ -13,19 +13,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AddChildListActivityFragment extends KidsListFargment {
-    private static final String NAVIGATION_ID = "navigationId";
-
-    public AddChildListActivityFragment() {
-    }
-
-    //Fragment는 Bundle로 데이터 주고 받음
-    public static AddChildListActivityFragment newInstance(int id) {
-        AddChildListActivityFragment fragment = new AddChildListActivityFragment();
-        Bundle args = new Bundle();
-        args.putInt(NAVIGATION_ID, id);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     // back stack에 있다가 돌아올 때 호출됨
     @Override
@@ -55,13 +42,5 @@ public class AddChildListActivityFragment extends KidsListFargment {
         kids.add(new Kid("테스트지롱", 2, "ㅇ", 2, 2, true));
         adapter.setKids(kids);
         adapter.notifyDataSetChanged();
-
-//        // TODO : 아이가 없을 때는 자녀 등록 화면으로 유도한다.
-//        if(SharedManager.getInstance().getUser().getChild().size() == 0) {
-//            Toast.makeText(getContext(), "자녀가 없어요.", Toast.LENGTH_SHORT).show();
-//        } else {
-//            adapter.setKids(SharedManager.getInstance().getUser().getChild());
-//        }
-//        adapter.notifyDataSetChanged();
     }
 }
