550d2eaf1d400eff611b7b3b0b87fbef14b9a0ee
lmjing <lmjing1234@gmail.com>
2018-12-06 14:05:50 +0900

    활동량 xml 변경

--- app/src/main/java/com/kt/iotheroes/kidscafesolution/TabActivity/Tab1Kids/DetailActivity/Viewholders/KidAcitivityViewHolder.java
+++ app/src/main/java/com/kt/iotheroes/kidscafesolution/TabActivity/Tab1Kids/DetailActivity/Viewholders/KidAcitivityViewHolder.java
@@ -1,9 +1,11 @@
 package com.kt.iotheroes.kidscafesolution.TabActivity.Tab1Kids.DetailActivity.Viewholders;
 
+import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.TextView;
 
 import com.hookedonplay.decoviewlib.DecoView;
+import com.hookedonplay.decoviewlib.charts.SeriesItem;
 import com.kt.iotheroes.kidscafesolution.R;
 import com.kt.iotheroes.kidscafesolution.Util.Dialog.ViewHolderParent;
 
@@ -12,6 +14,9 @@ import com.kt.iotheroes.kidscafesolution.Util.Dialog.ViewHolderParent;
  */
 
 public class KidAcitivityViewHolder extends ViewHolderParent {
+
+    private static final float seriesMax = 50f;
+    private static final String format = "%.0f%%";
 
     private TextView text_walk, text_cal;
     private DecoView graph_walk, graph_cal;
@@ -23,5 +28,25 @@ public class KidAcitivityViewHolder extends ViewHolderParent {
         text_cal = (TextView) itemView.findViewById(R.id.text_cal);
         graph_cal = (DecoView) itemView.findViewById(R.id.graph_cal);
         graph_walk = (DecoView) itemView.findViewById(R.id.graph_walk);
+
+        drawGraph(graph_walk);
+    }
+
+    public void addProgressListener(@NonNull final SeriesItem seriesItem, @NonNull final TextView textView, float maxValue) {
+        seriesItem.addArcSeriesItemListener(new SeriesItem.SeriesItemListener() {
+            @Override
+            public void onSeriesItemAnimationProgress(float percentComplete, float currentPosition) {
+                float percentFilled = (currentPosition - seriesItem.getMinValue()) / (seriesItem.getMaxValue() - seriesItem.getMinValue());
+                textView.setText(String.format(format, percentFilled * 100f));
+            }
+
+            @Override
+            public void onSeriesItemDisplayProgress(float percentComplete) {
+
+            }
+        });
+    }
+
+    public void drawGraph(DecoView graph) {
     }
 }
