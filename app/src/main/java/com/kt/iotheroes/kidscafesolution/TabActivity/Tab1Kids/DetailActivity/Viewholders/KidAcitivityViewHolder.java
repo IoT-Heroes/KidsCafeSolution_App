7cbe7c27fe0ec23f5c6c4bbc0b2a2ba38861fc32
lmjing <lmjing1234@gmail.com>
2018-12-06 14:27:45 +0900

    활동량 그래프 함수 생성

--- app/src/main/java/com/kt/iotheroes/kidscafesolution/TabActivity/Tab1Kids/DetailActivity/Viewholders/KidAcitivityViewHolder.java
+++ app/src/main/java/com/kt/iotheroes/kidscafesolution/TabActivity/Tab1Kids/DetailActivity/Viewholders/KidAcitivityViewHolder.java
@@ -1,11 +1,13 @@
 package com.kt.iotheroes.kidscafesolution.TabActivity.Tab1Kids.DetailActivity.Viewholders;
 
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.TextView;
 
 import com.hookedonplay.decoviewlib.DecoView;
 import com.hookedonplay.decoviewlib.charts.SeriesItem;
+import com.hookedonplay.decoviewlib.events.DecoEvent;
 import com.kt.iotheroes.kidscafesolution.R;
 import com.kt.iotheroes.kidscafesolution.Util.Dialog.ViewHolderParent;
 
@@ -15,24 +17,24 @@ import com.kt.iotheroes.kidscafesolution.Util.Dialog.ViewHolderParent;
 
 public class KidAcitivityViewHolder extends ViewHolderParent {
 
-    private static final float seriesMax = 50f;
+    private static final float seriesMax = 100f;
     private static final String format = "%.0f%%";
 
-    private TextView text_walk, text_cal;
+    private TextView text_percent_walk, text_percent_cal;
     private DecoView graph_walk, graph_cal;
 
     public KidAcitivityViewHolder(View itemView) {
         super(itemView);
 
-        text_walk = (TextView) itemView.findViewById(R.id.text_walk);
-        text_cal = (TextView) itemView.findViewById(R.id.text_cal);
+        text_percent_walk = (TextView) itemView.findViewById(R.id.text_percent_walk);
+        text_percent_cal = (TextView) itemView.findViewById(R.id.text_percent_cal);
         graph_cal = (DecoView) itemView.findViewById(R.id.graph_cal);
         graph_walk = (DecoView) itemView.findViewById(R.id.graph_walk);
 
         drawGraph(graph_walk);
     }
 
-    public void addProgressListener(@NonNull final SeriesItem seriesItem, @NonNull final TextView textView, float maxValue) {
+    public void addProgressListener(@NonNull final SeriesItem seriesItem, @NonNull final TextView textView) {
         seriesItem.addArcSeriesItemListener(new SeriesItem.SeriesItemListener() {
             @Override
             public void onSeriesItemAnimationProgress(float percentComplete, float currentPosition) {
@@ -48,5 +50,17 @@ public class KidAcitivityViewHolder extends ViewHolderParent {
     }
 
     public void drawGraph(DecoView graph) {
+        SeriesItem seriesItem1 = new SeriesItem.Builder(Color.argb(255, 64, 255, 64), Color.argb(255, 255, 0, 0))
+                .setRange(0, seriesMax, 0)
+                .setInitialVisibility(false)
+                .setLineWidth(50f)
+                .setCapRounded(true)
+                .setShowPointWhenEmpty(true)
+                .build();
+
+        int mSeriesIdx = graph.addSeries(seriesItem1);
+        addProgressListener(seriesItem1, text_percent_walk);
+
+        graph.addEvent(new DecoEvent.Builder(50).setIndex(mSeriesIdx).setDelay(500).build());
     }
 }
