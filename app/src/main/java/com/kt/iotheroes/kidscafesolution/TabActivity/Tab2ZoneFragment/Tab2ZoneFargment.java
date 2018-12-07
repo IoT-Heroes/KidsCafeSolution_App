5574d8acad7ae8c28b127c166007c44b24a1360f
lmjing <lmjing1234@gmail.com>
2018-12-07 15:29:25 +0900

    fragment 내 tab 구현 완료

--- app/src/main/java/com/kt/iotheroes/kidscafesolution/TabActivity/Tab2ZoneFragment/Tab2ZoneFargment.java
+++ app/src/main/java/com/kt/iotheroes/kidscafesolution/TabActivity/Tab2ZoneFragment/Tab2ZoneFargment.java
@@ -1,11 +1,9 @@
 package com.kt.iotheroes.kidscafesolution.TabActivity.Tab2ZoneFragment;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,10 +16,8 @@ import com.kt.iotheroes.kidscafesolution.TabActivity.Tab2ZoneFragment.Test.Tab2L
 public class Tab2ZoneFargment extends TabParentFragment {
     private static final String NAVIGATION_ID = "navigationId";
     private int navigationId;
-    private OnFragmentInteractionListener mListener;
 
     private TabLayout tabLayout;
-    private ViewPager viewPager;
 
     public Tab2ZoneFargment() {}
 
@@ -58,10 +54,12 @@ public class Tab2ZoneFargment extends TabParentFragment {
         tabLayout.addTab(tabLayout.newTab().setText("목록"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
+        FragmentTransaction childFt = getChildFragmentManager().beginTransaction();
+        childFt.add(R.id.child_fragment_container, new ZoneTab1MapFragment()).commit();
+
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-//                viewPager.setCurrentItem(tab.getPosition());
                 if (tab.getPosition() == 0)
                     setChildFragment(new ZoneTab1MapFragment());
                 else
@@ -78,29 +76,6 @@ public class Tab2ZoneFargment extends TabParentFragment {
 
             }
         });
-
-//        viewPager = (ViewPager)view.findViewById(R.id.view_pager);
-//
-//        ZoneTabPagerAdapter pagerAdapter = new ZoneTabPagerAdapter(getFragmentManager());
-//        viewPager.setAdapter(pagerAdapter);
-//        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-//
-//        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-//            @Override
-//            public void onTabSelected(TabLayout.Tab tab) {
-//                viewPager.setCurrentItem(tab.getPosition());
-//            }
-//
-//            @Override
-//            public void onTabUnselected(TabLayout.Tab tab) {
-//
-//            }
-//
-//            @Override
-//            public void onTabReselected(TabLayout.Tab tab) {
-//
-//            }
-//        });
     }
 
     private void setChildFragment(Fragment child) {
@@ -113,50 +88,8 @@ public class Tab2ZoneFargment extends TabParentFragment {
         }
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    // Activity에 할당되었을 때 호출되며 인자로 Activity를 넘겨준다.
-    //Activity에 이벤트 콜백을 설정
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-
-    // Fragment가 Activity로부터 할당이 제거될 때 호출
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
     @Override
     public void reload() {
 
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
