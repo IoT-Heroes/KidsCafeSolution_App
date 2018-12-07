2bd9a2dfb5a3e6856ec918650767d9ece53d1919
lmjing <lmjing1234@gmail.com>
2018-12-07 14:44:25 +0900

    zone tab 및 viewPagerAdapter 생성

---
+++ app/src/main/java/com/kt/iotheroes/kidscafesolution/TabActivity/Tab2ZoneFragment/Test/Tab1Map/ZoneTab1MapFragment.java
@@ -0,0 +1,23 @@
+package com.kt.iotheroes.kidscafesolution.TabActivity.Tab2ZoneFragment.Test.Tab1Map;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kt.iotheroes.kidscafesolution.R;
+
+public class ZoneTab1MapFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_zone_tab1_map, container, false);
+    }
+}
