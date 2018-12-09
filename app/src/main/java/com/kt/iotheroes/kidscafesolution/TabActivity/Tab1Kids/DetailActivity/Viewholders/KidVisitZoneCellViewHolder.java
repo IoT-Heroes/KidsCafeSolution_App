15e1eb0ada96aada7885aaef2a43c1f0df674165
lmjing <lmjing1234@gmail.com>
2018-12-09 10:42:08 +0900

    zoneViewholder class 생성

---
+++ app/src/main/java/com/kt/iotheroes/kidscafesolution/TabActivity/Tab1Kids/DetailActivity/Viewholders/KidVisitZoneCellViewHolder.java
@@ -0,0 +1,32 @@
+package com.kt.iotheroes.kidscafesolution.TabActivity.Tab1Kids.DetailActivity.Viewholders;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.kt.iotheroes.kidscafesolution.Model.UsingZone;
+import com.kt.iotheroes.kidscafesolution.R;
+import com.kt.iotheroes.kidscafesolution.Util.ParentView.ViewHolderParent;
+
+/**
+ * Created by mijeong on 2018. 12. 5..
+ */
+
+public class KidVisitZoneCellViewHolder extends ViewHolderParent {
+
+    ImageView image_color;
+    TextView text_room_name, text_room_count;
+
+    public KidVisitZoneCellViewHolder(View itemView) {
+        super(itemView);
+
+        image_color = (ImageView)itemView.findViewById(R.id.image_color);
+        text_room_name = (TextView)itemView.findViewById(R.id.text_room_name);
+        text_room_count = (TextView)itemView.findViewById(R.id.text_room_count);
+    }
+
+    public void initViewHolder(UsingZone zoneData) {
+        text_room_name.setText(zoneData.getZoneName());
+        text_room_count.setText(zoneData.getCount() + "회");
+    }
+}
