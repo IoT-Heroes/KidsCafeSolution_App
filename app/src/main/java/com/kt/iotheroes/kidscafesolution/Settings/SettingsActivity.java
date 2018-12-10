5841dd4f859c0f7b7ecd2a2bd4b794a221fa34cf
lmjing <lmjing1234@gmail.com>
2018-12-10 11:23:16 +0900

    settings activity 생성

---
+++ app/src/main/java/com/kt/iotheroes/kidscafesolution/Settings/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.kt.iotheroes.kidscafesolution.Settings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kt.iotheroes.kidscafesolution.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
