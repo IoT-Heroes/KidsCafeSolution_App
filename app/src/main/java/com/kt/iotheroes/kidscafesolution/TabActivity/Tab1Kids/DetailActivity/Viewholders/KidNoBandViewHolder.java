72068e2291b17c3c5dd7a6c1a9fba6be83a7c71f
lmjing <lmjing1234@gmail.com>
2018-12-15 23:39:35 +0900

    버튼 눌렸을 때 이벤트 발생되는 것 확인

--- app/src/main/java/com/kt/iotheroes/kidscafesolution/TabActivity/Tab1Kids/DetailActivity/Viewholders/KidNoBandViewHolder.java
+++ app/src/main/java/com/kt/iotheroes/kidscafesolution/TabActivity/Tab1Kids/DetailActivity/Viewholders/KidNoBandViewHolder.java
@@ -1,7 +1,9 @@
 package com.kt.iotheroes.kidscafesolution.TabActivity.Tab1Kids.DetailActivity.Viewholders;
 
+import android.app.Activity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.kt.iotheroes.kidscafesolution.ParentView.ViewHolderParent;
 import com.kt.iotheroes.kidscafesolution.R;
@@ -20,11 +22,12 @@ public class KidNoBandViewHolder extends ViewHolderParent {
         button_connect_band = (Button)itemView.findViewById(R.id.button_connect_band);
     }
 
-    public void initViewHolder() {
+    public void initViewHolder(final Activity activity) {
         button_connect_band.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // TODO : 키즈 밴드 연결 화면으로 넘어가기.
+                Toast.makeText(activity.getApplicationContext(), "버튼 눌림", Toast.LENGTH_SHORT).show();
             }
         });
     }
